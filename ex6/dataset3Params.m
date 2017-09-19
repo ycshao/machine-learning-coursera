3f963697fc6268358a03e7360c75c3ea19cfdaa5
Yuecheng Shao <ycshao0402@gmail.com>
2017-09-18 23:51:10 -0400

    ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,8 +8,6 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,7 +21,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_values = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_values = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+for i=1:length(C_values)
+    for j=1:length(sigma_values)
+        C = C_values(i);
+        sigma = sigma_values(j);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+        predictions = svmPredict(model, Xval);
+        predictions_error(i,j) = mean(double(predictions ~= yval));
+    end
+end
+
+mm = min(min(predictions_error));
+[i j] = find(predictions_error == mm);
+C = C_values(i)
+sigma = sigma_values(j)
 
 
 
--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,7 +97,8 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
+    index = find(strcmp(vocabList, str) == 1);
+    word_indices = [word_indices; index];
 
 
 
